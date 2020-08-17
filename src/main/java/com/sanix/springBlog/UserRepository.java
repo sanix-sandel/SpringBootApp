18fb3fb14eed39676060ef8aff8d0571544767a2
sanix sandel <sanicksikani@gmail.com>
2020-08-18 02:28:09 +0300

    implementing UsersRepository

---
+++ src/main/java/com/sanix/springBlog/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sanix.springBlog;
+
+import com.sanix.springBlog.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <UserEntity, Long>{
+
+    UserEntity findUserByEmail(String email);
+}
