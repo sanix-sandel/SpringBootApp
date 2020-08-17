be4714c86621e9cd4287a3f58dee0c29fe393971
sanix sandel <sanicksikani@gmail.com>
2020-08-18 00:37:40 +0300

    controllers created

--- src/main/java/com/sanix/springBlog/ui/controller/UserController.java
+++ src/main/java/com/sanix/springBlog/ui/controller/UserController.java
@@ -1,10 +1,28 @@
 package com.sanix.springBlog.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping()
+@RequestMapping("users")
 public class UserController {
 
+    @GetMapping
+    public String getUser(){
+        return "get user called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
 }
