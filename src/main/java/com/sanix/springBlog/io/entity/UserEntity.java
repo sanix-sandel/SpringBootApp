90387a00a6eb9c0cc5884bf2be4c25f73905d574
sanix sandel <sanicksikani@gmail.com>
2020-08-18 02:40:32 +0300

    Autowire USerServiceImpl to UserRepository

--- src/main/java/com/sanix/springBlog/io/entity/UserEntity.java
+++ src/main/java/com/sanix/springBlog/io/entity/UserEntity.java
@@ -20,6 +20,70 @@ public class UserEntity implements Serializable {
     @Column(nullable=false, length=50)
     private String firstName;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean getEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+
     @Column(nullable=false, length=50)
     private String lastName;
 
--- src/main/java/com/sanix/springBlog/service/impl/UserServiceImpl.java
+++ src/main/java/com/sanix/springBlog/service/impl/UserServiceImpl.java
@@ -1,12 +1,34 @@
 package com.sanix.springBlog.service.impl;
 
+import com.sanix.springBlog.UserRepository;
+import com.sanix.springBlog.io.entity.UserEntity;
 import com.sanix.springBlog.service.UserService;
 import com.sanix.springBlog.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user){
-        return null;
+
+        UserEntity userEntity=new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+
+        UserEntity storeUserDetails=userRepository.save(userEntity);
+
+
+
+        UserDto returnValue=new UserDto();
+        BeanUtils.copyProperties(storeUserDetails, returnValue);
+
+        return returnValue;
     };
 }
