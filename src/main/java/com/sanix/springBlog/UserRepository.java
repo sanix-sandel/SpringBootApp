6587d1fb6011fb34bdc953db9c4f3492d06449ba
sanix sandel <sanicksikani@gmail.com>
2020-08-18 03:53:44 +0300

    check if user already exists

--- src/main/java/com/sanix/springBlog/UserRepository.java
+++ src/main/java/com/sanix/springBlog/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository <UserEntity, Long>{
 
-    UserEntity findUserByEmail(String email);
+    UserEntity findByEmail(String email);
 }
