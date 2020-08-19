06da5e27f4df562bc74ab7d1a8c61aef37b3297e
sanix sandel <sanicksikani@gmail.com>
2020-08-19 03:24:05 +0300

    Implementing Load User By Username

--- src/main/java/com/sanix/springBlog/service/impl/UserServiceImpl.java
+++ src/main/java/com/sanix/springBlog/service/impl/UserServiceImpl.java
@@ -7,10 +7,13 @@ import com.sanix.springBlog.shared.Utils;
 import com.sanix.springBlog.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -46,7 +49,12 @@ public class UserServiceImpl implements UserService {
     };
 
     @Override
-    public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if(userEntity==null) throw new UsernameNotFoundException(email);
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
 }
